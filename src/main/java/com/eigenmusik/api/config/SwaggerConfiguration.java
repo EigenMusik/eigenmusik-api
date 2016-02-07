f7f24f51e1ba7bb6ec66f7ff5e080bdf4967a4fa
Tim Coulson <tim@justpark.com>
2016-02-07 14:22:51 +0000

    Use internally managed swagger UI

--- src/main/java/com/eigenmusik/api/config/SwaggerConfiguration.java
+++ src/main/java/com/eigenmusik/api/config/SwaggerConfiguration.java
@@ -5,13 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.security.Principal;
+import java.util.List;
 
 import static com.google.common.base.Predicates.or;
+import static com.google.common.collect.Lists.newArrayList;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
@@ -23,7 +27,34 @@ public class SwaggerConfiguration {
                 .select()
                 .paths(apiPaths())
                 .build()
+                .securitySchemes(newArrayList(bearerToken()))
+                .securityContexts(newArrayList(securityContext()))
                 .ignoredParameterTypes(Principal.class, Pageable.class);
+    }
+
+    private ApiKey bearerToken() {
+        return new ApiKey(bearerSchema, "bearer", "header");
+    }
+
+    public static final String bearerSchema = "bearer";
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    public AuthorizationScope global()
+    {
+        return new AuthorizationScope("global", "accessEverything");
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = global();
+        return newArrayList(
+                new SecurityReference(bearerSchema, authorizationScopes));
     }
 
     private Predicate<String> apiPaths() {
--- src/main/java/com/eigenmusik/api/home/HomeController.java
+++ src/main/java/com/eigenmusik/api/home/HomeController.java
@@ -1,12 +1,36 @@
 package com.eigenmusik.api.home;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternUtils;
+import org.springframework.hateoas.Resources;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.support.ServletContextResourcePatternResolver;
+
+import javax.servlet.ServletContext;
+import java.io.IOException;
 
 @Controller
 public class HomeController {
+
+    private static Logger log = Logger.getLogger(HomeController.class);
+    // TODO I don't like having versions hard coded.
+    private static final String SWAGGER_UI_RESOURCE_PATH = "webjars/eigenmusik-swagger-ui/2.1.4/index.html";
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    private ApplicationContext ctx;
+
     @RequestMapping("/")
-    public String home() {
-        return "redirect:swagger-ui.html";
+    public String home() throws IOException {
+        return "redirect:" + SWAGGER_UI_RESOURCE_PATH;
     }
 }
